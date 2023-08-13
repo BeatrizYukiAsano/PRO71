9ff3104ff2f91645f80087f513b0e7de0d089a65
BeatrizYukiAsano <113141229+BeatrizYukiAsano@users.noreply.github.com>
2023-08-13 09:16:01 -0300

    Add files via upload

---
+++ PRO_1-1_C71_TemplateDoProjeto-main (1)/PRO_1-1_C71_TemplateDoProjeto-main/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  const firebaseConfig = {
+  apiKey: "AIzaSyBRqtDcgRz4AzH-k2WCE6m-k1_yoqn9hXk",
+  authDomain: "ciclistaeletronico-559a3.firebaseapp.com",
+  projectId: "ciclistaeletronico-559a3",
+  storageBucket: "ciclistaeletronico-559a3.appspot.com",
+  messagingSenderId: "731275807685",
+  appId: "1:731275807685:web:0c9f090b161ee47f99d9ac",
+  measurementId: "G-DC1VV3SMXD"
+};
+
+  
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
